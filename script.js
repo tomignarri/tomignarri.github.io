46b306197fb308a7a52c43efe0cbc5efec1c074d
govtbodies <tom.ignarri@gmail.com>
2020-02-07 10:31:04 -0500

    animate about/contact button

--- script.js
+++ script.js
@@ -3,4 +3,5 @@
 // Show info and contact on click.
 function activateInfo(){
     document.querySelector(".contactStart").classList.toggle("contactEnd");
+    document.querySelector(".navButtonStart").classList.toggle("navButtonEnd");
 };    
