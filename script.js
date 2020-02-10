71630ada43338d37e0e0d0e9004deb46f0835f14
govtbodies <tom.ignarri@gmail.com>
2020-02-10 14:59:16 -0500

    add scroll to top on nav button click

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 // Show info and contact on click.
 function activateInfo(){
+    document.body.scrollTop = 0;
     document.querySelector(".contactStart").classList.toggle("contactEnd");
     document.querySelector(".navButtonStart").classList.toggle("navButtonEnd");
 }; 
