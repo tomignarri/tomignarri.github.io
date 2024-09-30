dc9d5b17eeb006f469d4145bab8c58c19f99a4e2
tomignarri <tom.ignarri@gmail.com>
2024-09-30 12:15:24 -0400

    removed other content

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 
 
-// Show info and contact on click.
-function activateInfo(){
-    document.body.scrollTop = 0;
-    document.querySelector(".contactStart").classList.toggle("contactEnd");
-    document.querySelector(".navButtonStart").classList.toggle("navButtonEnd");
-}; 
+ 
 
 
 
