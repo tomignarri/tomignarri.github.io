e9d71af2143cb4a346299670b81061b11352ce74
tomignarri <tom.ignarri@gmail.com>
2025-05-22 10:59:20 -0400

    added a bg color and stubbed out controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,24 +1,27 @@
-import { Float, Environment, useGLTF, OrbitControls } from '@react-three/drei'
-
+import { Text, Html, ContactShadows, PresentationControls, Float, Environment, useGLTF } from '@react-three/drei'
 
 export default function Experience()
 {
-    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
 
     return <>
 
-        <Environment preset="city" />
-
         <color args={ [ '#241a1a' ] } attach="background" />
 
-        <OrbitControls makeDefault />
+        <Environment preset="city" />
+        
+        <PresentationControls 
+            global
+            rotation={ [ 0.13, 0.1, 0 ] }
+            polar={ [ - 0.4, 0.2 ] }
+            azimuth={ [ - 1, 0.75 ] }
+            config={ { mass: 2, tension: 400 } }
+            snap
+        >
+   
+        </PresentationControls>
 
-        <Float>
-            <primitive
-                object={ computer.scene }
-                position-y={ - 1.2 }
-            />
-        </Float>
+
+
 
     </>
 }
