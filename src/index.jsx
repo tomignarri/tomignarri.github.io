a52c0e6878927863ff2ad5b23fe821d7fcb564b8
tomignarri <tom.ignarri@gmail.com>
2025-06-03 15:14:24 -0400

    camera adjustments

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
+import Card from './Card.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
@@ -12,9 +13,11 @@ root.render(
             fov: 45,
             near: 0.1,
             far: 2000,
-            position: [ -3, 1.5, 4 ]
+            position: [ 0, 0, 0 ]
         } }
     >
         <Experience />
+        <gridHelper args={[10, 10]} />
+     
     </Canvas>
 )
