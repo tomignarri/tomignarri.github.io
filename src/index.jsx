e3f57060fe369c8a0e030b4493747e4844dd3a3e
tomignarri <tom.ignarri@gmail.com>
2025-06-04 15:32:51 -0400

    repositoned camera

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,31 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
+import { Html } from '@react-three/drei'
 import Experience from './Experience.jsx'
 import Card from './Card.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
+
     <Canvas
         className="r3f"
+        orthographic
         camera={ {
             fov: 45,
+            zoom: 100,
             near: 0.1,
-            far: 2000,
-            position: [ 0, 0, 0 ]
+            far: 200,
+            position: [ 0, 7, 0 ]
         } }
+
+        
     >
         <Experience />
         <gridHelper args={[10, 10]} />
+
+        
      
     </Canvas>
 )
