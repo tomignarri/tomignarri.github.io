af4b172f4d26f32dbdbb0474ce72b4fae369d5ee
tomignarri <tom.ignarri@gmail.com>
2025-06-09 12:31:40 -0400

    implemented working version of zoom

--- src/Zoom.jsx
+++ src/Zoom.jsx
@@ -1,12 +1,18 @@
 import { useEffect } from 'react'
+import { useThree } from '@react-three/fiber'
 
 
 
 export default function Zoom()
 {
+    const { camera } = useThree();
+
     useEffect(() => {
         const handleScroll = (e) => {
-            console.log(e.deltaY)
+            console.log("first ", camera.zoom)
+            camera.zoom += e.deltaY * -0.1
+            camera.zoom = Math.max(30, Math.min(300, camera.zoom))
+            camera.updateProjectionMatrix()
         }
         
         window.addEventListener('wheel', handleScroll)
