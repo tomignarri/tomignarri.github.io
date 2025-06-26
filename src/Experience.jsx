d90ade7d094ad84459d984b04d95c960a1253103
tomignarri <tom.ignarri@gmail.com>
2025-06-26 16:59:02 -0400

    neurocrine and arcutis content

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -84,7 +84,7 @@ export default function Experience()
             <p>
                 This campaign was one I contributed to during my time at AbelsonTaylor.
                 Working closely with my colleagues, we played a key role in the digital execution of an 
-                educational initiative showcasing advances in gene therapy for Hemophilia B treatment.
+                educational initiative showcasing advances in gene therapy for Hemophilia&nbsp;B&nbsp;treatment.
             </p>
             <video autoPlay loop muted playsInline><source src="videos/hemevolution-video-1.mp4" type="video/mp4" /></video>
         </article>
@@ -100,24 +100,64 @@ export default function Experience()
 
     const arcutisLinks = <>
         <a href="https://pharmawards.com/2024-arcutis-dtp-cream-that-can/#website" target='_blank'>View campaign</a>
+        <br />
+        <a href="https://www.zoryve.com/save-with-zoryve-direct">View the website</a>
     </>
 
     const arcutisContent = <>
         <article>
             <p>
                 Arcutis's topical product, Zoryve, was a large campaign encompassing many digital executions.
-                I was a part of a variety of emails and online ads, but my primary project for this campaign was a website for both patients and healthcare providers.
-                This was a project that was full of challenging problems and tight deadlines, where I was often using tools with which I did not have a lot of experience.
+                I was a part of a variety of emails and online ads, but my primary project for this campaign was a 
+                website for both patients and healthcare providers.
+                This was a project that was full of challenging problems and tight deadlines, where I was often using 
+                tools with which I did not have a lot of experience.
+            </p>
+            <video autoPlay loop muted playsInline><source src="videos/arcutis-video-1.mp4" type="video/mp4" /></video>
+        </article>
+        <article>
+            <p>
+                Part of this project which I was primarily resposible for was the front-end of a system for patients to register 
+                for copay cards for the product.
+                Successful communication with the IT team of our client and an external vendor was pivotal. We would 
+                frequently coordinate together to diagnose and fix issues 
+                with the system. The website has since been out of the agency's hands, but the form that I built is still as 
+                it was. You can see it on the "View the webiste" link&nbsp;above.
+            </p>
+            <img alt="arcutis-image-1" src="images/arcutis-image-1.png" />
+        </article>
+    </>
+
+
+    const neurocrineLinks = <>
+        <a href="https://www.morethanhd.com/">View More Than HD</a>
+    </>
+
+
+    const neurocrineContent = <>
+        <article>
+            <p>
+                Neurocrine was one client that had a particularly important. I was involved with of variety of
+                websites, Interactive Visual Aids (IVA), email campaigns, and online banner advertisements. The largest of 
+                these was a brand called Ingrezza. These projects went through many different iterations and additions. Fast turn around 
+                time was essential, as was coordination with my developer colleagues and other teams across due to the large volume of 
+                content and detail needed for this brand.
             </p>
         </article>
         <article>
             <p>
-                Part of this project which I was primarily resposible for was the front-end of a system for patients to register for copay cards for the product.
-                Successful communication with the IT team of our client and an external vendor was pivotal. We would frequently coordinate together to diagnose and fix issues 
-                with the system.
+                One project for this client where I was able to utilize my knowledge in Node JS, was for an educational site to serve 
+                patients, healthcare providers and whomever, to learn about Huntington's Disease. The title of the project is More Than HD.
+                This was built from the ground up using vanilla javascript and NodeJS. We built an anonymous, dynamic polling system to collect data 
+                about users of the site using that Node backend.JS
             </p>
         </article>
     </>
+
+
+
+
+
 
 
     
@@ -148,7 +188,7 @@ export default function Experience()
         />
      
         <Card 
-            title="Neurocrine/Ingrezza" 
+            title="Neurocrine" 
             position={ myCardPositions.neuro }
             techUsed="Javascript, HTML, SCSS, ASP.NET, C#, React, NodeJS"
         />
