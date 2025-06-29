61598fffcd0f1c1f1aee47eefc5aa666267cf6c0
tomignarri <tom.ignarri@gmail.com>
2025-06-29 14:47:02 -0400

    added zoom state

--- src/ZoomIntro.jsx
+++ src/ZoomIntro.jsx
@@ -1,22 +1,24 @@
-import { memo, useEffect } from 'react'
+import { memo, useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const ZoomIntro = memo(function ZoomIntro({ introPartDone, setIntroPartDone }) {
+    console.log('MyComponent rendered');
+    const [zoomIsReached, setZoomIsReached] = useState(false);
 
     useEffect(() => {
         const zoomIntroContainer = document.querySelector('#zoom-intro');
         const parentOverlay = document.querySelector('.intro-container');
+        
 
-        let zoomAmountReached = false;
         let zoomAmount = 0;
 
         const onZoom = (e) => {
             zoomAmount += Math.abs(e.deltaY)
-            if(zoomAmount > 200 && !zoomAmountReached) {
+            if(zoomAmount > 200 && !zoomIsReached) {
                 zoomIntroContainer.style.opacity = '0';
                 zoomIntroContainer.style.pointerEvents = 'none';
-                zoomAmountReached = true;
+                setZoomIsReached(true)
                 if (introPartDone) {
                     parentOverlay.style.opacity = '0';
                 }
