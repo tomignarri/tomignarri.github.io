c806847452aea3cf39990d460553adbc1f8822d6
tomignarri <tom.ignarri@gmail.com>
2025-07-22 15:29:53 -0400

    implmented intersection observer

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,12 @@
-import { store } from "./store";
 import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 export default function Card({ title, techUsed, links, content }) {
+
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+
   useEffect(() => {
     const portfolioVideos = document.querySelectorAll(".card video");
 
@@ -14,16 +19,22 @@ export default function Card({ title, techUsed, links, content }) {
 
   return (
     <>
-      <div className="card-container">
+      <div ref={ref} className="card-container">
         <section className="card">
           <section className="card-content">
             <h1>{title}</h1>
             <h2>{techUsed}</h2>
             <aside>{links}</aside>
-            {content}
+            {inView ? (
+              content
+            ) : (
+              <p>loading</p>
+            )}
+            
           </section>
         </section>
       </div>
+ 
     </>
   );
 }
